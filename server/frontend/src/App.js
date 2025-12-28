c0ce8980d2dc4c96b73f3d61c489669e80b0393f
Akisha <akdabesamis@mymail.mapua.edu.ph>
2025-12-28 03:56:53 -0500

    Save all current work before pulling

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
+import Register from "./components/Register/Register";
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
-  );
-}
-export default App;
+    return (
+      <Routes>
+        <Route path="/login" element={<LoginPanel />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    );
+  }
+  export default App;
+  
